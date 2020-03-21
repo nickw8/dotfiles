074cf268d04724f787829dbbf83aac945d127959
Nick Weight <nweight@mychg.com>
2020-03-20 18:17:43 -0600

    changed dotfile alias

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,3 +3,4 @@ alias cleardns="sudo killall -HUP mDNSResponder"
 
 # Set up alias for managing dotfiles with git
 alias dotfiles='/usr/bin/git --git-dir=/Users/nweight/.dotfiles/ --work-tree=/Users/nweight'
+alias git.dot='/usr/bin/git --git-dir=/Users/nweight/.dotfiles/ --work-tree=/Users/nweight'
