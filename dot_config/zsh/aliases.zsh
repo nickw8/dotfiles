1ed4d72bcd681dc6e398ceca82275799617b6ecc
nickw8 <nicholas.weight@gmail.com>
2024-11-02 17:56:24 -0600

    readding config directory

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,10 @@
+alias k=kubectl
+alias kx=kubectx
+alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias ll="eza --all --grid --group-directories-first --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
+alias l='eza -blF'
+alias la='eza -gla'
+alias cat='bat'
+alias gpat='git push && git push --tags'
+alias bump='cz bump --yes'
+alias tf='terraform'
