bbd49e95ba9d2508acdc2a9ba9ea44e08e6fd968
nickw8 <nicholas.weight@gmail.com>
2024-11-02 17:46:49 -0600

    yes

--- dot_zshrc.d/aliases.zsh
+++
@@ -1,8 +0,0 @@
-alias k=kubectl
-alias kx=kubectx
-alias ls="eza --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias ll="eza --all --grid --group-directories-first --color=always --long --git --no-filesize --icons=always --no-time --no-user --no-permissions"
-alias cat='bat'
-alias gpat='git push && git push --tags'
-alias bump='cz bump --yes'
-alias tf='terraform'
